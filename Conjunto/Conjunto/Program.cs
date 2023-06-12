442c049e3274a9064e3e5cf2e171f4bd6da664f2
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-06-12 18:31:44 -0300

    Add files via upload

---
+++ Conjunto/Conjunto/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Conjunto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            HashSet<int> a = new HashSet<int>();
+            for(int i = 0; i < n; i++)
+            {
+                a.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            HashSet<int> b = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                b.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            HashSet<int> c = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                c.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> d = new HashSet<int>();
+            d.UnionWith(a);
+            d.UnionWith(b);
+            d.UnionWith(c);
+
+            Console.WriteLine("Total students: " + d.Count);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
