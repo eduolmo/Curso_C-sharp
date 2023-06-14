d01c9cccb3c1aeab642f222b0a4bd1ccd9ac4890
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-06-13 22:15:45 -0300

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> votos = new Dictionary<string, int>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var linha = sr.ReadLine().Split(',');
+
+                        if (votos.ContainsKey(linha[0]))
+                        {
+                            votos[linha[0]] += int.Parse(linha[1]);
+                        }
+                        else
+                        {
+                            votos[linha[0]] = int.Parse(linha[1]);
+                        }
+                    }
+                    
+                    foreach(var item in votos)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO: " + ex.Message);
+            }
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
